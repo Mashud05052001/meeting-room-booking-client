f508381451ce3d2219034a7d5613d3f6eab582e9
Mashud05052001 <masudmahi0505@gmail.com>
2024-09-18 03:56:04 +0600

    home banner update

--- src/components/pagesComponents/home/HomeBanner.tsx
+++ src/components/pagesComponents/home/HomeBanner.tsx
@@ -23,8 +23,8 @@ const HomeBanner = () => {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: false,
   };
 
